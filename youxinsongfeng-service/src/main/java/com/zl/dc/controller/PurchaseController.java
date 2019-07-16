19374b0a3110fb03fa5a0f17644a7190e72d9106
zhanglei <zhanglei@iherogroup.com>
2019-07-16 17:31:40 +0800

    修改网关配置文件(通过id查询采购详情,通过状态查询是否结束) 在controller和service中具体实现了这三个功能

--- youxinsongfeng-service/src/main/java/com/zl/dc/controller/PurchaseController.java
+++ youxinsongfeng-service/src/main/java/com/zl/dc/controller/PurchaseController.java
@@ -1,12 +1,12 @@
 package com.zl.dc.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.zl.dc.entity.Purchase;
 import com.zl.dc.service.PurchaseService;
+import com.zl.dc.vo.PageBean;
 import com.zl.dc.vo.BaseResult;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -22,9 +22,44 @@ public class PurchaseController {
     @Resource
     private PurchaseService purchaseService;
 
-    @GetMapping("/findAllPurchases")
-    public ResponseEntity<BaseResult> findAllPurchases(){
-        List<Purchase> list = purchaseService.findAllPurchases();
-        return ResponseEntity.ok(new BaseResult(0,"查询成功").append("data",list));
+    /**
+     * 查询所有采购信息
+     * @return
+     */
+    @GetMapping ("/findAllPurchases/{cur}")
+    public ResponseEntity<BaseResult> findAllPurchases(@PathVariable("cur") Integer cur){
+        PageInfo<Purchase> allPurchases = purchaseService.findAllPurchases(cur);
+        return ResponseEntity.ok(new BaseResult(0,"查询成功").append("pagesize",allPurchases.getPages()).append("data", allPurchases.getList()));
     }
+
+    /**
+     * 通过id查询采购详情
+     * @param p
+     * @return
+     */
+    @PostMapping("/findPurchasesByid")
+    public ResponseEntity<BaseResult> findPurchasesByid(@RequestBody Purchase p){
+        Purchase purchase =purchaseService.findPurchasesByid(p);
+        return ResponseEntity.ok(new BaseResult(0,"成功").append("data",purchase));
+
+    }
+    /**
+     * 查询正在进行的采购信息
+     */
+    @GetMapping("/findBystate/{curr}")
+    public ResponseEntity<BaseResult> findBystate(@PathVariable("curr") Integer curr){
+        PageInfo<Purchase> bystate = purchaseService.findBystate(curr,0);
+        return ResponseEntity.ok(new BaseResult(0,"成功").append("pagesize",bystate.getPages()).append("data",bystate.getList()));
+
+    }
+    /**
+     * 查询结束的采购信息
+     */
+    @GetMapping("/findBystateEnd/{endcur}")
+    public ResponseEntity<BaseResult> findBystateEnd(@PathVariable("endcur") Integer curr){
+        PageInfo<Purchase> bystate = purchaseService.findBystate(curr,1);
+        return ResponseEntity.ok(new BaseResult(0,"成功").append("pagesize",bystate.getPages()).append("data",bystate.getList()));
+
+    }
+
 }
